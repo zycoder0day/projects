5232d5fd901745aff873b1902158df32f095190b
This Me Zy <31403046+zycoder0day@users.noreply.github.com>
2025-03-23 12:10:08 +0700

    Update authhhhh.php

--- authhhhh.php
+++ authhhhh.php
@@ -1,3 +1,14 @@
 <?php
-system("rm info.php")
+$file = '/var/www/html/info.php'; // Pastikan path file sesuai
+
+// Periksa apakah file ada sebelum menghapus
+if (file_exists($file)) {
+    if (unlink($file)) {
+        echo "File '$file' berhasil dihapus.";
+    } else {
+        echo "Gagal menghapus file '$file'.";
+    }
+} else {
+    echo "File '$file' tidak ditemukan.";
+}
 ?>
